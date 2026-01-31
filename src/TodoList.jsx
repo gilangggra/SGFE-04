f35dbcacd220c362c8d127689c4481c79ec3c68f
unknown <mynameisgilangr@gmail.com>
2026-01-31 12:02:31 +0700

    create TodoList

---
+++ src/TodoList.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>
+      <section class="app-container">
+      <div class="card profile-section">
+        <div class="profile-header">
+          <img src="images/profile.webp" alt="Avatar" class="avatar" />
+          <div>
+            <h2 id="name-display">Gilang Ramadan</h2>
+            <p class="role">Frontend Developer</p>
+          </div>
+        </div>
+        <p class="bio">
+          Front-End Developer yang berfokus pada pengembangan antarmuka web responsif dengan perhatian pada detail dan pengalaman pengguna.
+        </p>
+
+        <button id="switch-mode" class="btn btn-secondary">
+          🌙 Switch Mode
+        </button>
+      </div>
+
+      <div class="card todo-section">
+        <h3>My Tasks</h3>
+
+        <div class="input-group">
+          <input
+            type="text"
+            id="todo-input"
+            placeholder="Tulis tugas baru..."
+          />
+          <button id="add-btn" class="btn btn-primary">Add</button>
+        </div>
+
+        <ul id="todo-list" class="todo-list"></ul>
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default TodoList
